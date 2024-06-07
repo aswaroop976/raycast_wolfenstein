b54b302be7c04a311583067178ddba9056163c8d
Arpan Swaroop <arpan.swaroop@gmail.com>
2024-06-07 18:04:42 -0500

    raycasting working, but havent implemented the player moving around yet

--- main.c
+++ main.c
@@ -71,6 +71,11 @@ int done() {
   return 0;
 }
 
+void verLine(SDL_Renderer *renderer, int x, int y1, int y2, SDL_Color color) {
+  SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+  SDL_RenderDrawLine(renderer, x, y1, x, y2);
+}
+
 int main(int argc, char *argv[]) {
   double posX = 22, posY = 12;      // x and y start position
   double dirX = -1, dirY = 0;       // initial direction vector
@@ -109,6 +114,8 @@ int main(int argc, char *argv[]) {
       double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
       double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
 
+      double perpWallDist;
+
       // what direction to step in x or y-direction (either +1 or -1)
       int stepX;
       int stepY;
@@ -130,6 +137,78 @@ int main(int argc, char *argv[]) {
         stepY = 1;
         sideDistY = (mapY + 1.0 - posY) * deltaDistY;
       }
+      // perform DDa
+      while (hit == 0) {
+        // jump to next map square, either in the x-direction, or in the
+        // y-direction
+        if (sideDistX < sideDistY) {
+          sideDistX += deltaDistX;
+          mapX += stepX;
+          side = 0;
+        } else {
+          sideDistY += deltaDistY;
+          mapY += stepY;
+          side = 1;
+        }
+        // check if ray has hit a wall
+        if (worldMap[mapX][mapY] > 0)
+          hit = 1;
+      }
+      // Calculate distance projected on camera direction. This is the shortest
+      // distance from the point where the wall is hit to the camera plane.
+      // Euclidean to center camera point would give fisheye effect! This can be
+      // computed as (mapX - posX + (1 - stepX) / 2) / rayDirX for side == 0, or
+      // same formula with Y for size == 1, but can be simplified to the code
+      // below thanks to how sideDist and deltaDist are computed: because they
+      // were left scaled to |rayDir|. sideDist is the entire length of the ray
+      // above after the multiple steps, but we subtract deltaDist once because
+      // one step more into the wall was taken above.
+      if (side == 0) {
+        perpWallDist = (sideDistX - deltaDistX);
+      } else {
+        perpWallDist = (sideDistY - deltaDistY);
+      }
+      // calculate height of line to draw on screen
+      int lineHeight = (int)(screenHeight / perpWallDist);
+
+      // calculate lowest and hightest pixel to fill in current stripe
+      int drawStart = -lineHeight / 2 + screenHeight / 2;
+      if (drawStart < 0) {
+        drawStart = 0;
+      }
+      int drawEnd = lineHeight / 2 + screenHeight / 2;
+      if (drawEnd >= screenHeight) {
+        drawEnd = screenHeight - 1;
+      }
+
+      SDL_Color color;
+      switch (worldMap[mapX][mapY]) {
+      case 1:
+        color = (SDL_Color){255, 0, 0, 255};
+        break; // Red
+      case 2:
+        color = (SDL_Color){0, 255, 0, 255};
+        break; // Green
+      case 3:
+        color = (SDL_Color){0, 0, 255, 255};
+        break; // Blue
+      case 4:
+        color = (SDL_Color){255, 255, 255, 255};
+        break; // White
+      default:
+        color = (SDL_Color){255, 255, 0, 255};
+        break; // Yellow
+      }
+
+      // Give x and y sides different brightness
+      if (side == 1) {
+        color.r /= 2;
+        color.g /= 2;
+        color.b /= 2;
+      }
+
+      // Draw the pixels of the stripe as a vertical line
+      verLine(renderer, x, drawStart, drawEnd, color);
     }
 
     SDL_RenderPresent(renderer); // update the screen
